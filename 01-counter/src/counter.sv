8fe11d85d047e5ff3d0ae95f0ac3ff955d33b768
menaja248-netizen <menaja248@gmail.com>
2026-06-05 11:43:25 +0530

    Update counter.sv

--- 01-counter/src/counter.sv
+++ 01-counter/src/counter.sv
@@ -18,7 +18,24 @@ module updown_counter(
     input logic [3:0] d_in,// Input data for loading
     output logic [3:0] count // Counter output
 );
-
+always_ff @(posedge clk or negedge rst_n)
+  begin
+    if(!rst_n)
+    begin
+       count<=4'b0000;
+    end
+    else if(load)
+    begin
+      count<=d_in;
+    end
+    else if(enable)
+    begin
+      if(up_down)
+        count<=count+1'b1;
+      else
+        count<=count-1'b1;
+    end
+  end
 
 endmodule
 
